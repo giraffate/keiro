5f3b7c8a9773f613add72ad9af85404dbff78f43
Takayuki Nakata <f.seasons017@gmail.com>
2021-03-19 22:13:01 +0900

    struct handler

--- examples/helloworld.rs
+++ examples/helloworld.rs
@@ -1,14 +1,20 @@
+use std::future::Future;
 use std::net::SocketAddr;
+use std::pin::Pin;
 
 use hyper::{Body, Request, Response, Server};
 
-use keiro::{Params, Router};
+use keiro::{Handler, Params, Router};
 
 #[tokio::main]
 async fn main() {
     let mut router = Router::new();
+    let about_handler = AboutHandler {
+        message: "Hello from a struct handler!".to_string(),
+    };
     router.get("/", index);
     router.get("/hello/:user", hello);
+    router.get("/about", about_handler);
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
 
     Server::bind(&addr)
@@ -27,4 +33,18 @@ async fn hello(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
         "Hello, {}!",
         params.find("user").unwrap()
     ))))
+}
+
+struct AboutHandler {
+    message: String,
+}
+
+impl Handler for AboutHandler {
+    fn call(
+        &self,
+        _req: Request<Body>,
+    ) -> Pin<Box<dyn Future<Output = Result<Response<Body>, hyper::Error>> + Send + Sync>> {
+        let message = self.message.clone();
+        Box::pin(async { Ok(Response::new(Body::from(message))) })
+    }
 }
